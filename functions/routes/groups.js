2ace6c56ef1e95fce264dd2fcfc0f4f89d612a91
TakahiroSono <taka.sono889@gmail.com>
2018-11-28 17:21:52 +0900

    fix it so that it can be deployed

--- functions/routes/groups.js
+++ functions/routes/groups.js
@@ -8,60 +8,22 @@ app.use(bodyParser.json());
 var router = express.Router();
 
 // setup firestore
-admin.initializeApp(functions.config().firebase);
+// admin.initializeApp(functions.config().firebase);
 var db = admin.firestore();
 
 /* Get user list */
 router.get('/:name?', function(req, res, next) {
-  var userInfo = req.query.name;
-  var userRef = db.collection('users').doc(userInfo);
-  var getDoc = userRef.get()
-    .then(doc => {
-      if (!doc.exists) {
-        res.json({"message": "No such document!"});
-      } else {
-        res.json({"message": "success", [userInfo]: doc.data()});
-      }
-    })
-    .catch(err => {
-      res.json({"message": err});
-    });
 });
 
 /* Post new user*/
 router.post('/', function(req, res, next) {
-  var newData = req.body
-  var docRef = db.collection('users').doc(newData.name);
-  docRef.set(newData).then(ref => {
-    res.json({"message":'success'});
-  }).catch(function (error) {
-    res.json({"message": error})
-    next(error);
-  });
 });
 
 /* Update user */
 router.put('/:name?', function(req, res, next) {
-  var userInfo = req.query.name;
-  var updateData = req.body
-  var userRef = db.collection('users').doc(userInfo);
-  userRef.update(updateData).then(ref => {
-    res.json({"message":'success'});
-  }).catch(function (error) {
-    res.json({"message": error})
-    next(error);
-  });
 });
 
 router.delete('/:name?', function(req, res, next){
-  var userInfo = req.query.name;
-  var userRef = db.collection('users').doc(userInfo);
-  userRef.delete().then(ref => {
-    res.json({"message": "success"});
-  }).catch(function (error){
-    res.json({"message": error})
-    next(error);
-  });
 });
 
 module.exports = router;
