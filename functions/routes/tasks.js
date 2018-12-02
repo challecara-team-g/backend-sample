08ecc3b4849af886862760345a65b0b6bff83c6d
katsuma <katsu.sai1028@gmail.com>
2018-12-02 17:55:09 +0900

    change api of task

--- functions/routes/tasks.js
+++ functions/routes/tasks.js
@@ -10,19 +10,57 @@ var router = express.Router();
 // setup firestore
 var db = admin.firestore();
 
-/* Get user list */
+/* Get task list */
 router.get('/:name?', function(req, res, next) {
+  var userInfo = req.query.name;
+  var userRef = db.collection('tasks').doc(userInfo);
+  var getDoc = userRef.get()
+    .then(doc => {
+      if (!doc.exists) {
+        res.json({"message": "No such document!"});
+      } else {
+        res.json({"message": "success", [userInfo]: doc.data()});
+      }
+    })
+    .catch(err => {
+      res.json({"message": err});
+    });
 });
 
-/* Post new user*/
+/* Post new tasks*/
 router.post('/', function(req, res, next) {
+  var newData = req.body
+  var docRef = db.collection('tasks').doc(newData.id);
+  docRef.set(newData).then(ref => {
+    res.json({"message":'success'});
+  }).catch(function (error) {
+    res.json({"message": error})
+    next(error);
+  });
 });
 
-/* Update user */
+/* Update tasks */
 router.put('/:name?', function(req, res, next) {
+  var userInfo = req.query.name;
+  var updateData = req.body
+  var userRef = db.collection('tasks').doc(userInfo);
+  userRef.update(updateData).then(ref => {
+    res.json({"message":'success'});
+  }).catch(function (error) {
+    res.json({"message": error})
+    next(error);
+  });
 });
 
 router.delete('/:name?', function(req, res, next){
+  var userInfo = req.query.name;
+  var userRef = db.collection('tasks').doc(userInfo);
+  userRef.delete().then(ref => {
+    res.json({"message": "success"});
+  }).catch(function (error){
+    res.json({"message": error})
+    next(error);
+  });
 });
 
 module.exports = router;
